903aa9733f0d424c999f5a173c6b3b448404c01e
MrDonalds <229090982@qq.com>
2019-06-02 22:14:49 +0800

    delet test and add a example usage of the async

--- www/app.py
+++ www/app.py
@@ -20,10 +20,10 @@ def index(request):
 async def init(loop):      #为了提高并发性能，使用异步协程
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000) # loop 也是异步io里的东西
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000) # 挂起异步调用接口
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
+loop = asyncio.get_event_loop()   # 创建事件循环
+loop.run_until_complete(init(loop)) # 将协程注册到loop，并启动 loop
 loop.run_forever()
