0b9b6ae71f5588ad2a69c966921cc84db3dd9c5a
MrDonalds <229090982@qq.com>
2019-06-03 19:43:38 +0800

    chang the user name and password

--- www/app.py
+++ www/app.py
@@ -110,7 +110,7 @@ def datetime_filter(t):
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 async def init(loop):
-    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www', password='www', db='awesome')
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='password', db='awesome')
     app = web.Application(loop=loop, middlewares=[
         logger_factory, response_factory
     ])
@@ -124,3 +124,5 @@ async def init(loop):
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
+
+
